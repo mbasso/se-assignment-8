a7fed0a87a147d6471bbad3305e08d52fabde7ab
Matteo Basso <matteo.basso@usi.ch>
2019-11-27 19:23:35 +0100

    project setup

---
+++ code/assignment8/src/assignment8/model/Address.java
@@ -0,0 +1,102 @@
+package assignment8.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="addresses")
+public class Address {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="id")
+	private int id;
+
+    @Column(name="country")
+	private String country;
+    
+    @Column(name="city")
+	private String city;
+    
+    @Column(name="street")
+	private String street;
+
+	public Address(int id, String country, String city, String street) {
+		this.id = id;
+		this.country = country;
+		this.city = city;
+		this.street = street;
+	}
+
+	public Address(String country, String city, String street) {
+		this.country = country;
+		this.city = city;
+		this.street = street;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+	
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	public String getStreet() {
+		return street;
+	}
+	
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + ((country == null) ? 0 : country.hashCode());
+		result = prime * result + ((street == null) ? 0 : street.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Address other = (Address) obj;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (country == null) {
+			if (other.country != null)
+				return false;
+		} else if (!country.equals(other.country))
+			return false;
+		if (street == null) {
+			if (other.street != null)
+				return false;
+		} else if (!street.equals(other.street))
+			return false;
+		return true;
+	}
+
+}
--- code/assignment8/src/exception/ExistingUserException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class ExistingUserException extends Exception {
-
-	private static final long serialVersionUID = 246956539128533553L;
-
-}
